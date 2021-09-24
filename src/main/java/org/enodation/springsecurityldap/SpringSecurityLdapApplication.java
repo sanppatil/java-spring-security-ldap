c9d3402128d50163645a0c613a5ba59b0cc1893f
Sandip Patil <sanppatil@gmail.com>
2021-09-23 23:54:59 -0400

    Added changes

---
+++ src/main/java/org/enodation/springsecurityldap/SpringSecurityLdapApplication.java
@@ -0,0 +1,15 @@
+package org.enodation.springsecurityldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class SpringSecurityLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLdapApplication.class, args);
+	}
+
+}
